06ca488dd544db999881d54d040831214eb3076b
Brian Craycraft <craycraftb24@hotmail.com>
2020-05-12 04:41:51 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
-def oxford_comma(array)
 
+
+def oxford_comma(array)
+  
+  
+  if array.length >= 3
+    [array[0...-1].join(", "), array.last].join(", and ")
+  else
+    array.join
+    array.join(" and ")
+  end
+  
+  
+  
 end
