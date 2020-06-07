f48221eaf1923e80a57455d676c93ce5d64dc678
Brian Craycraft <craycraftb24@hotmail.com>
2020-06-07 04:11:39 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,7 @@
 
-
 def oxford_comma(array)
   
+<<<<<<< HEAD
   
   if array.length >= 3
     [array[0...-1].join(", "), array.last].join(", and ")
@@ -9,6 +9,10 @@ def oxford_comma(array)
     array.join
     array.join(" and ")
   end
+=======
+  fruits = array.join
+  array.join(",")
+>>>>>>> 2a61f8f56d85f7726140ed66d84da84403ff42c6
   
   
   
